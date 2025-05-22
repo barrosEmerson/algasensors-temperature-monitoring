cc69f2e2debb8718736269b4b22dd8e0fda82295
Emerson Barros <emerson.barrostech@hotmail.com>
2025-05-22 20:15:10 -0300

    save

---
+++ src/main/java/com/barrositcompany/algasenrors/tempature/monitoring/infrastructure/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.barrositcompany.algasenrors.tempature.monitoring.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String FANOUT_EXCHANGE_NAME = "temperature-processing.temperature-received.v1.e";
+
+
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Queue queue(){
+        return QueueBuilder.durable("temperature-monitoring.process-temperature.v1.q").build();
+    }
+
+    public FanoutExchange exchange(){
+        return ExchangeBuilder.fanoutExchange(FANOUT_EXCHANGE_NAME).build();
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange());
+    }
+}
